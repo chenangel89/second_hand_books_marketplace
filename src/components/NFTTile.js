c45b9eb06e2e898957b11aa414fc006c9fb1be18
chenangel89 <76103256+chenangel89@users.noreply.github.com>
2023-06-01 17:47:16 +0800

    Update NFTTile.js

--- src/components/NFTTile.js
+++ src/components/NFTTile.js
@@ -6,25 +6,27 @@ import {
   import { GetIpfsUrlFromPinata } from "../utils";
 
 function NFTTile (data) {
+
     const newTo = {
         pathname:"/nftPage/"+data.data.tokenId
     }
 
-    const IPFSUrl = GetIpfsUrlFromPinata(data.data.image);
+    const IPFSUrl_image = GetIpfsUrlFromPinata(data.data.image);
 
     return (
         <Link to={newTo}>
-        <div className="border-2 ml-12 mt-5 mb-12 flex flex-col items-center rounded-lg w-48 md:w-72 shadow-2xl">
-            <img src={IPFSUrl} alt="" className="w-72 h-80 rounded-lg object-cover" crossOrigin="anonymous" />
-            <div className= "text-white w-full p-2 bg-gradient-to-t from-[#454545] to-transparent rounded-lg pt-5 -mt-20">
-                <strong className="text-xl">{data.data.name}</strong>
-                <p className="display-inline">
-                    {data.data.description}
-                </p>
+          <div className="border-2  mt-10 mb-10 flex flex-col items-center rounded-lg w-48 md:w-72 shadow-5xl m-3">
+            <img src={IPFSUrl_image} alt="" className="w-72 h-80 rounded-lg object-cover" crossOrigin="anonymous" />
+            <div className="text-white w-full p-2 bg-gradient-to-t from-[#454545] to-transparent rounded-lg pt-5 -mt-20">
+              <strong className="text-xl">{data.data.name}</strong>
+              <p className="display-inline">
+                {data.data.description}
+              </p>
             </div>
-        </div>
+          </div>
         </Link>
-    )
+      );
+      
 }
 
 export default NFTTile;
